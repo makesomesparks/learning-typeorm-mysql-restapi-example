d2dfb2d9c7777ae9d3a2e24832cb8d82ada1a523
Taegyun, Ko <dev@ctrls.to>
2021-11-04 18:51:04 +0900

    check undefined route

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,24 @@ createConnection()
 
     Routes.forEach((route) =>
     {
-      (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) =>
-      {
-        if (route.permission != "guest") {
-          // Permission; somthing codes ...
-        }
+      if (typeof ((app as any)[route.method]) !== 'undefined') {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) =>
+        {
+          if (route.permission != "guest") {
+            // Permission; somthing codes ...
+          }
 
-        const result = new (route.controller as any)()[route.action](req, res, next);
+          const result = new (route.controller as any)()[route.action](req, res, next);
 
-        if (result instanceof Promise) {
-          result.then((result) => result !== null && result !== undefined ? res.send(result) : undefined);
+          if (result instanceof Promise) {
+            result.then((result) => result !== null && result !== undefined ? res.send(result) : undefined);
+          }
+          else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
         }
-        else if (result !== null && result !== undefined) {
-          res.json(result);
-        }
+        );
       }
-      );
     });
 
     app.listen(3000);
