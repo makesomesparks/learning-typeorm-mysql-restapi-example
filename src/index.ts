489dca93e3c169e41db770c8a00c048bae063ee7
Taegyun, Ko <dev@ctrls.to>
2021-11-07 00:47:09 +0900

    remove deprecated bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,47 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
-import { createConnection } from 'typeorm';
 import { Request, Response } from 'express';
-import { Routes } from './routes';
 import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { Routes } from './routes';
+import { StringUtils } from './util/StringUtils';
 
 createConnection()
   .then(async (connection) =>
   {
     const app = express();
-    app.use(bodyParser.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
     Routes.forEach((route) =>
     {
-      if (typeof ((app as any)[route.method]) !== 'undefined') {
+      let x = new (route.controller as any)()[route.action];
+
+      console.log(`[${typeof x !== 'undefined' ? 'O' : ' '}] ${StringUtils.rightPad(route.method.toUpperCase(), 6)} ${route.route}`);
+
+      if (typeof x !== 'undefined')
+      {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) =>
         {
-          if (route.permission != "guest") {
+          if (route.permission != "guest")
+          {
             // Permission; somthing codes ...
           }
 
-          const result = new (route.controller as any)()[route.action](req, res, next);
+          if (typeof route.controller[route.action] !== 'undefined')
+          {
+            const result = new (route.controller as any)()[route.action](req, res, next);
 
-          if (result instanceof Promise) {
-            result.then((result) => result !== null && result !== undefined ? res.send(result) : undefined);
+            if (result instanceof Promise)
+            {
+              result.then((result) => result !== null && result !== undefined ? res.send(result) : undefined);
+            }
+            else if (result !== null && result !== undefined)
+            {
+              res.json(result);
+            }
           }
-          else if (result !== null && result !== undefined) {
-            res.json(result);
-          }
-        }
-        );
+        });
       }
     });
 
