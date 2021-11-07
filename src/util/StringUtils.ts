ffdf44c78bf9c675a44e95c7d4a16b8de1ada1d7
Taegyun, Ko <dev@ctrls.to>
2021-11-07 21:54:53 +0900

    update StringUtils

--- src/util/StringUtils.ts
+++ src/util/StringUtils.ts
@@ -47,4 +47,58 @@ export const StringUtils =
         break;
     }
   },
+  regex:
+  {
+    isValidEmail: (email: string): boolean =>
+    {
+      if (typeof email === 'undefined' || email === null)
+      {
+        return false;
+      }
+
+      // length for ccTLDs, handshake domain
+      let regex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{1,30}$/;
+
+      if (regex.test(email) !== true)
+      {
+        return false;
+      }
+
+      return true;
+    },
+    isValidId: (id: string): boolean =>
+    {
+      if (typeof id === 'undefined' || id === null)
+      {
+        return false;
+      }
+
+      // length 6-50, e.g. "0000abcd", "taegyun.ko", "ctrl-s", "help_me"
+      let regex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z]){5,49}$/;
+
+      if (regex.test(id) !== true)
+      {
+        return false;
+      }
+
+      return true;
+    },
+    isValidPassword: (password: string): boolean =>
+    {
+      if (typeof password === 'undefined' || password === null)
+      {
+        return false;
+      }
+
+      // length 7-70, must contain at least one number and special character, e.g. 'asdf44^', '1122ws@@'
+      let regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*\W).{7,70}$/;
+
+      if (regex.test(password) !== true)
+      {
+        return false;
+      }
+
+      return true;
+    }
+  },
 };
