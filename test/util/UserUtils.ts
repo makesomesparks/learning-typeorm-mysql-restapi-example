aeddcebcae33d3e55728ba82b342057e7904cddf
Taegyun, Ko <dev@ctrls.to>
2021-11-12 16:09:45 +0900

    First Test Unit(UserUtils)

--- test/util/UserUtils.ts
+++ test/util/UserUtils.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { UserUtils } from "src/util/UserUtils";
+import { UserRepository } from "src/repository/user/UserRepository";
 import { createConnection } from "typeorm";
 
 describe('UserUtils', () =>
@@ -7,7 +7,22 @@ describe('UserUtils', () =>
   it('checkExistUserEmail("dev@ctrls.to")', async () =>
   {
     await createConnection(require("ormconfig"));
-    let a: any = await UserUtils.checkExistUserEmail("dev@ctrls.to");
+    let a: any = await UserRepository.getUserEmail("dev@ctrls.to");
     expect(a).to.not.equal(undefined);
   });
+
+  it('checkExistUserId("ctrls")', async () =>
+  {
+    // await createConnection(require("ormconfig"));
+    // let a: any = await UserUtils.checkExistUserEmail("dev@ctrls.to");
+    // expect(a).to.not.equal(undefined);
+  });
+
+  it('checkExistReserve("ctrls")', async () =>
+  {
+    // await createConnection(require("ormconfig"));
+    // let a: any = await UserUtils.checkExistUserEmail("dev@ctrls.to");
+    // expect(a).to.not.equal(undefined);
+  });
+
 });
